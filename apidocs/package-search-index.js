11c377759fc0173151615384e8780b05c540c316
Travis CI User <travis@example.org>
2020-05-15 22:38:43 +0000

    Site checkin for project OpenWMS.org CORE: UAA

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.openwms.core"},{"l":"org.openwms.core.app"},{"l":"org.openwms.core.configuration"},{"l":"org.openwms.core.search"},{"l":"org.openwms.core.uaa"},{"l":"org.openwms.core.uaa.impl"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.openwms.core"},{"l":"org.openwms.core.app"},{"l":"org.openwms.core.configuration"},{"l":"org.openwms.core.search"},{"l":"org.openwms.core.uaa"},{"l":"org.openwms.core.uaa.api"},{"l":"org.openwms.core.uaa.impl"}]
