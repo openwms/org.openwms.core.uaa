c535ae205b56d3521723b03219f4ec8913e26253
Heiko Scherrer <hscherrer@interface21.io>
2021-06-22 00:09:08 +0200

    Moved test class

--- src/test/java/org/openwms/core/uaa/UAAStartupTest.java
+++ src/test/java/org/openwms/core/uaa/UAAStartupTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openwms.core;
+package org.openwms.core.uaa;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
