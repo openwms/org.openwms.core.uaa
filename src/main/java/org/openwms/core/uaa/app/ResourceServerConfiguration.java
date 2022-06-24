8aa4d9268bbff0237bbf21d87d844c96f4dccbd7
Heiko Scherrer <hscherrer@interface21.io>
2022-06-24 20:39:53 +0200

    #22 Instead of explicitly configuring the Resource Server, nowadays we configure a second SecurityFilterChain with an order right behind the Auth Server SecurityFilterChain

--- src/main/java/org/openwms/core/uaa/app/ResourceServerConfiguration.java
+++ src/main/java/org/openwms/core/uaa/app/ResourceServerConfiguration.java
@@ -15,12 +15,16 @@
  */
 package org.openwms.core.uaa.app;
 
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * A ResourceServerConfiguration.
@@ -28,12 +32,33 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
  * @author Heiko Scherrer
  */
 @Configuration
-@EnableResourceServer
-class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+//@EnableResourceServer
+class ResourceServerConfiguration/* extends ResourceServerConfigurerAdapter*/ {
 
     @Value("${owms.security.successUrl}")
     private String successUrl;
 
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .oauth2ResourceServer().jwt()
+                //.formLogin(Customizer.withDefaults())
+        ;
+        return http.build();
+    }
+
+    @Bean // <3>
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+    /*
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.sessionManagement()
@@ -47,4 +72,6 @@ class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
                 .formLogin().defaultSuccessUrl(successUrl)
         ;
     }
+
+     */
 }
