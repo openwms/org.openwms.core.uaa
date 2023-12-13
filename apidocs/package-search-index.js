5a7d481dae239105a8820518458b72113d53b3d8
CI Server <ci@example.com>
2023-12-13 13:16:38 +0000

    Site checkin for project OpenWMS.org CORE: UAA

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.openwms.core.uaa"},{"l":"org.openwms.core.uaa.api"},{"l":"org.openwms.core.uaa.app"},{"l":"org.openwms.core.uaa.impl"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.openwms.core.uaa"}];updateSearchResults();
